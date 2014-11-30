d5e1e0d4d882666f2b9b1d822102f281dfc58357
Andrew Poydence <apoydence@gmail.com>
2014-11-30 06:15:56 -0700

    Removes unused function.

--- hydra_suite_test.go
+++ hydra_suite_test.go
@@ -3,7 +3,6 @@ package hydra_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"reflect"
 
 	"testing"
 )
@@ -11,8 +10,4 @@ import (
 func TestHydra(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Hydra Suite")
-}
-
-func samePointers(a interface{}, b interface{}) bool {
-	return reflect.ValueOf(a).Pointer() == reflect.ValueOf(b).Pointer()
 }
