dc114df05bba2fe52df6786b8cd0ee2a55ff12c2
Andrew Poydence <apoydence@gmail.com>
2014-12-09 13:37:44 -0700

    go fmt

--- examples/wordCount/wordCount.go
+++ examples/wordCount/wordCount.go
@@ -1,75 +1,75 @@
 package wordCount
 
-import(
+import (
+	"bufio"
 	. "github.com/apoydence/hydra/examples/wordCount/types"
 	"github.com/apoydence/hydra/types"
 	"os"
-	"bufio"
 	"strings"
 )
 
-func PathProducer(sf types.SetupFunction, argv []string){
+func PathProducer(sf types.SetupFunction, argv []string) {
 	out := sf.SetName("PathProducer").AsProducer().Build()
 	defer close(out)
-	
-	for _, path := range argv{
+
+	for _, path := range argv {
 		out <- NewStringMarshaler(path)
 	}
 }
 
-func PathValidator(sf types.SetupFunction){
+func PathValidator(sf types.SetupFunction) {
 	in, out := sf.AsFilter("PathProducer").Build()
 	defer close(out)
-	
-	for path := range in{
-		if _, err := os.Stat(ToString(path)); err == nil{
+
+	for path := range in {
+		if _, err := os.Stat(ToString(path)); err == nil {
 			out <- path
-		} 
+		}
 	}
 }
 
-func WordExtractor(sf types.SetupFunction){
+func WordExtractor(sf types.SetupFunction) {
 	in, out := sf.AsFilter("github.com/apoydence/hydra/examples/wordCount.PathValidator").Build()
 	defer close(out)
 
-	for path := range in{
+	for path := range in {
 		p := ToString(path)
 		f, err := os.Open(p)
-		if err == nil{
+		if err == nil {
 			scanner := bufio.NewScanner(f)
 			scanner.Split(bufio.ScanWords)
-			for scanner.Scan(){
+			for scanner.Scan() {
 				out <- NewStringMarshaler(scanner.Text())
 			}
 		}
-		
-	}	
+
+	}
 }
 
-func SymbolRemover(sf types.SetupFunction){
+func SymbolRemover(sf types.SetupFunction) {
 	in, out := sf.AsFilter("github.com/apoydence/hydra/examples/wordCount.WordExtractor").Build()
 	defer close(out)
 
-	for word := range in{
+	for word := range in {
 		str := make([]byte, 0)
 		bytes := []byte(strings.ToLower(ToString(word)))
-		for _, x := range bytes{
-			if (x >= 0x30 && x <= 0x39) || (x >= 0x61 && x <= 0x7a){
+		for _, x := range bytes {
+			if (x >= 0x30 && x <= 0x39) || (x >= 0x61 && x <= 0x7a) {
 				str = append(str, x)
 			}
 		}
-		if len(str) > 0{
+		if len(str) > 0 {
 			out <- NewStringMarshaler(string(str))
 		}
 	}
 }
 
-func WordCounter(sf types.SetupFunction){
+func WordCounter(sf types.SetupFunction) {
 	in, out := sf.AsFilter("github.com/apoydence/hydra/examples/wordCount.SymbolRemover").Build()
 	defer close(out)
 	m := make(map[string]uint32)
 
-	for word := range in{
+	for word := range in {
 		incMap(ToString(word), 1, m)
 	}
 
@@ -80,25 +80,24 @@ func WordPrinter(sf types.SetupFunction, done chan struct{}) {
 	defer close(done)
 	in := sf.AsConsumer("github.com/apoydence/hydra/examples/wordCount.WordCounter").Build()
 	m := make(map[string]uint32)
-	
-	for wordMap:= range in{
-		for k, v := range ToMap(wordMap){
+
+	for wordMap := range in {
+		for k, v := range ToMap(wordMap) {
 			incMap(k, v, m)
 		}
 	}
 
-	for k, v := range m{
+	for k, v := range m {
 		println(k, v)
 	}
 }
 
-func incMap(word string, incBy uint32, m map[string]uint32){
+func incMap(word string, incBy uint32, m map[string]uint32) {
 	var i uint32
 	var ok bool
-	if i, ok = m[word]; !ok{
+	if i, ok = m[word]; !ok {
 		i = 0
 	}
 	i = i + incBy
 	m[word] = i
 }
-
--- examples/wordCountMain.go
+++ examples/wordCountMain.go
@@ -1,30 +1,30 @@
 package main
 
-import(
-	"github.com/apoydence/hydra"
-	"github.com/apoydence/hydra/types"
-	"github.com/apoydence/hydra/examples/wordCount"
-	"os"
+import (
 	"fmt"
+	"github.com/apoydence/hydra"
+	"github.com/apoydence/hydra/examples/wordCount"
+	"github.com/apoydence/hydra/types"
+	"os"
 )
 
-func main(){
+func main() {
 	args := os.Args
-	if len(args) == 0{
+	if len(args) == 0 {
 		fmt.Printf("usage: %s [Paths...]\n", args[0])
 		os.Exit(1)
 	}
 
-	producer := func(sf types.SetupFunction){
+	producer := func(sf types.SetupFunction) {
 		wordCount.PathProducer(sf, args[1:])
 	}
 
 	done := make(chan struct{})
-	cp := func(sf types.SetupFunction){
+	cp := func(sf types.SetupFunction) {
 		wordCount.WordPrinter(sf, done)
 	}
 
 	hydra.NewSetupScaffolding()(producer, wordCount.PathValidator, wordCount.WordExtractor, cp, wordCount.SymbolRemover, wordCount.WordCounter)
 
-	<- done
+	<-done
 }
